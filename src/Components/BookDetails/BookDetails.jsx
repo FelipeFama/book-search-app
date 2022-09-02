3fda5201f8282121855b83cf20502771d0d1c845
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-09-02 17:40:24 -0300

    I added react router and initial setup

---
+++ src/Components/BookDetails/BookDetails.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './BookDetails.css';
+
+const BookDetails = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default BookDetails
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import './main.css';
+import Home  from "./pages/Home/Home";
+import About from "./pages/About/About";
+import BookList from "./Components/BookList/BookList";
+import BookDetails from "./Components/BookDetails/BookDetails";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+   <Routes>
+     <Route path="/" element = { <Home /> }>
+      <Route path="about" element = { <About /> } />
+      <Route path="book" element = { <BookList /> } />
+      <Route path="/book/:id" element = {<BookDetails />} />
+     </Route>
+   </Routes>
+  </BrowserRouter>
 )
