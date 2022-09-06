c8dbc13e661d13e4a234d410b557781aebf8b531
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-09-06 19:54:01 -0300

    coding searchForm

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
         <div className="header-content flex flex-c text-center text-white">
           <h2 className="header-title text-capitalize">find your book of choice</h2><br/>
           <p className="header-text fs-18 w-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos facilis quisquam eveniet! Repellat facere sed tenetur nulla debitis laudantium, itaque esse optio natus magnam, eum qui ullam fugiat unde?</p>
+          <SearchForm />
         </div>
-        <SearchForm />
       </header>
     </div>
   )
