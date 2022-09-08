2721b544f713ec95d27b52bc43127b34c6abf69c
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-09-08 19:25:53 -0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { AppProvider } from './context';
 import './main.css';
 import Home  from "./pages/Home/Home";
 import About from "./pages/About/About";
-import BookList from "./components/BookList/BookList";
+import { BookList } from "./components/BookList/BookList";
 import BookDetails from "./components/BookDetails/BookDetails";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
