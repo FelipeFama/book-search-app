bb5b7ff14c3db64d9b763324893d9c75d7797d08
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-09-08 14:57:49 -0300

    add about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import aboutImg from "../../assets/book-shelf.jpg"
 import './About.css';
 
 const About = () => {
   return (
-    <div>kkkk</div>
+    <section className="about">
+      <div className="container">
+        <div className="section-title">
+          <h2>About</h2>
+        </div>
+
+        <div className="about-content grid">
+          <div className="about-img">
+            <img src= {aboutImg} alt="library stand"/>
+          </div>
+          <div className="about-text">
+            <h2 className="about-title fs-26 ls-1">About Bookhub</h2>
+            <p className="fs-17">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum sed in illo rem cumque, quaerat fugiat aliquam quis, voluptates quas, doloremque laudantium earum libero. Iure maiores delectus dolorum rem sequi.</p>
+            <p className="fs-17">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae perferendis libero, alias nobis quaerat totam illo maxime, iusto dolorem ducimus molestias consequuntur optio! Natus asperiores recusandae repellendus explicabo obcaecati.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
