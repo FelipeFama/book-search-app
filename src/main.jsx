813c9310796073c8d89855ceadee10ac0c1636ce
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-09-08 19:14:52 -0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AppProvider } from "./context";
-import "./main.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context';
+import './main.css';
 import Home  from "./pages/Home/Home";
 import About from "./pages/About/About";
 import BookList from "./components/BookList/BookList";
